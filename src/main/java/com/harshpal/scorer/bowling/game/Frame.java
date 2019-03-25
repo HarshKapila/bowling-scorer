9aa1a12f5342120b166ed4af4d80d75e810f701e
HarshKapila <harshkapila1@gmail.com>
2019-03-25 04:38:41 +0100

    remove unused @JsonIgnore

--- src/main/java/com/harshpal/scorer/bowling/game/Frame.java
+++ src/main/java/com/harshpal/scorer/bowling/game/Frame.java
@@ -2,24 +2,19 @@ package com.harshpal.scorer.bowling.game;
 
 import static com.harshpal.scorer.bowling.scoring.ScoringServiceImpl.MAX_HIT;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 public class Frame {
     private Integer first;
     private Integer second;
     private Integer extra;
 
-    @JsonIgnore
     public boolean isStrike() {
         return first == MAX_HIT;
     }
 
-    @JsonIgnore
     public boolean isSpare() {
         return first != null && second != null && (first + second >= MAX_HIT || second == MAX_HIT);
     }
 
-    @JsonIgnore
     public boolean isFirstChance() {
         return first == null;
     }
